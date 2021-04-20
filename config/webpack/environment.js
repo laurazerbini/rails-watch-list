6820f87719e58cbdcbe2373bf020e79afe2fd203
laura zerbini <lauraczerbini@gmail.com>
2021-04-20 21:27:07 +0200

    movie list

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,6 @@
-const { environment } = require('@rails/webpacker')
+// const { environment } = require('@rails/webpacker')
 
-module.exports = environment
+// module.exports = environment
 
 // config/webpack/environment.js
 const { environment } = require('@rails/webpacker')
